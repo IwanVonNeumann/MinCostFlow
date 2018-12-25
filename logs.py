578d484c5dfc073982449722aef9302302210101
IwanVonNeumann <grabageheap@inbox.lv>
2018-12-25 13:12:05 +0200

    logging refactored

--- logs.py
+++ logs.py
@@ -1,5 +1,4 @@
 def log_successful_step(step_number, G_inc, path, flow_inc, Flow, flow_value):
-    print('-' * 50)
     print('step', step_number)
     print('incremental:')
     print(G_inc)
@@ -8,12 +7,13 @@ def log_successful_step(step_number, G_inc, path, flow_inc, Flow, flow_value):
     print('flow:')
     print(Flow)
     print('flow value:', flow_value)
+    print('-' * 50)
 
 
 def log_unsuccessful_step(step_number):
-    print('-' * 50)
     print('step', step_number)
     print('No path found. Current flow is maximal')
+    print('-' * 50)
 
 
 def log_results(Flow, flow_value, total_cost):
